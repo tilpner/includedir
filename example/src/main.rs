b71188c952f0f4665d359a63c1d716d00fe752ab
Till Höppner <till@hoeppner.ws>
2016-09-15 23:51:46 +0200

    Upgrade dependencies, bump version

--- example/src/main.rs
+++ example/src/main.rs
@@ -5,10 +5,8 @@ include!(concat!(env!("OUT_DIR"), "/data.rs"));
 
 fn main() {
     println!("{:?}", FILES.get("data/foo"));
+
     for name in FILES.file_names() {
         println!("Found: {}", name);
     }
-    // for (k, v) in FILES.entries() {
-    //    println!("{}: {} bytes", k, v.len());
-    // }
 }
