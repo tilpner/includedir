fc04e3d0fd90b9fc2897df2f27245960b9151b10
tilpner <till@hoeppner.ws>
2018-03-12 15:41:15 +0100

    Fix test by sorting vector

--- example/src/main.rs
+++ example/src/main.rs
@@ -15,8 +15,9 @@ fn main() {
 fn test() {
     assert_eq!(FILES.get("data/foo").expect("data/foo not present").into_owned(), &[102, 111, 111, 10]);
 
-    let files = FILES.file_names().collect::<Vec<_>>();
-    assert_eq!(files, vec!["data/inner/boom", "data/foo", "data/empty"]);
+    let mut files = FILES.file_names().collect::<Vec<_>>();
+    files.sort();
+    assert_eq!(files, vec!["data/empty", "data/foo", "data/inner/boom"]);
 
     assert_eq!(FILES.get("data/inner/boom").unwrap().into_owned(), &[98, 111, 111, 109, 10]);
     assert_eq!(FILES.get("data/foo").unwrap().into_owned(), &[102, 111, 111, 10]);
