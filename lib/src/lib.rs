e6daad34230f40ea7dedf5a845fbfde2fe9da14b
Akos Vandra <axos88@gmail.com>
2018-12-11 21:38:16 +0100

    make use of GZip a compile time error if flate2 is missing

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -14,6 +14,7 @@ use flate2::bufread::GzDecoder;
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum Compression {
     None,
+    #[cfg(feature = "flate2")]
     Gzip
 }
 
@@ -95,8 +96,6 @@ impl Files {
                     Compression::None => Ok(Box::new(Cursor::new(b.1))),
                     #[cfg(feature = "flate2")]
                     Compression::Gzip => Ok(Box::new(GzDecoder::new(Cursor::new(b.1)))),
-                    #[cfg(not(feature = "flate2"))]
-                    Compression::Gzip => panic!("Feature 'flate2' not enabled"),
                 }
             }
             None => Err(Error::new(ErrorKind::NotFound, "Key not found")),
